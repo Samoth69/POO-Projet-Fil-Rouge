456e7c6e7ad4f44a0e725e8773f8b89a496c91fe
Thomas Violent <thomasviolent@gmail.com>
2021-10-25 15:18:32 +0200

    fin atelier 1 / début atelier 2

--- src/model/ModelImplementor.java
+++ src/model/ModelImplementor.java
@@ -1,6 +1,7 @@
 package model;
 
 
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 
@@ -8,140 +9,141 @@ import nutsAndBolts.PieceSquareColor;
 
 /**
  * @author francoise.perrin
- * 
- * Cete classe fabrique et stocke toutes les PieceModel du Model dans une collection 
+ * <p>
+ * Cete classe fabrique et stocke toutes les PieceModel du Model dans une collection
  * elle est donc responsable de rechercher et mettre � jour les PieceModel (leur position)
- * En r�alit�, elle d�l�gue � une fabrique le soin de fabriquer les bonnes PieceModel 
+ * En r�alit�, elle d�l�gue � une fabrique le soin de fabriquer les bonnes PieceModel
  * avec les bonnes coordonn�es
- * 
+ * <p>
  * En revanche, elle n'est pas responsable des algorithme m�tiers li�s au d�placement des pi�ces
  * (responsabilit� de la classe Model)
  */
 public class ModelImplementor {
 
-	// la collection de pi�ces en jeu - m�lange noires et blanches
-	private Collection<PieceModel> pieces ;	
+    // la collection de pi�ces en jeu - m�lange noires et blanches
+    private Collection<PieceModel> pieces;
 
-	public ModelImplementor() {
-		super();
+    public ModelImplementor() {
+        super();
 
-		pieces = ModelFactory.createPieceModelCollection();
-	}
+        pieces = ModelFactory.createPieceModelCollection();
+    }
 
-	public PieceSquareColor getPieceColor(Coord coord) {
-		PieceSquareColor color = null;
-		PieceModel piece = this.findPiece(coord);
+    public PieceSquareColor getPieceColor(Coord coord) {
+        PieceSquareColor color = null;
+        PieceModel piece = this.findPiece(coord);
 
-		if (piece != null) {
-			color = piece.getPieceColor();
-		}
-		return color;
-	}
+        if (piece != null) {
+            color = piece.getPieceColor();
+        }
+        return color;
+    }
 
-	public boolean isPiecehere(Coord coord) {
-		return this.findPiece(coord) != null;
-	}
+    public boolean isPiecehere(Coord coord) {
+        return this.findPiece(coord) != null;
+    }
 
-	public boolean isMovePieceOk(Coord initCoord, Coord targetCoord, boolean isPieceToTake) {
+    public boolean isMovePieceOk(Coord initCoord, Coord targetCoord, boolean isPieceToTake) {
 
-		boolean isMovePieceOk = false;
-		PieceModel initPiece = this.findPiece(initCoord);
-		if (initPiece != null) {
-			isMovePieceOk = initPiece.isMoveOk(targetCoord, isPieceToTake ) ;
-		}
-		return isMovePieceOk;
-	}
-
-
-	public boolean movePiece(Coord initCoord, Coord targetCoord) {
-
-		boolean isMovePieceDone = false;
-		PieceModel initPiece = this.findPiece(initCoord);
-		if (initPiece != null) {
-
-			// d�placement pi�ce
-			initPiece.move(targetCoord) ;
-			isMovePieceDone = true;
-		}
-		return isMovePieceDone;
-	}
-
-	public void removePiece(Coord pieceToTakeCoord) {
-
-		// TODO Atelier 2
-		
-	}
-
-	
-	public List<Coord> getCoordsOnItinerary(Coord initCoord, Coord targetCoord) {
-		List<Coord> coordsOnItinerary = null;
-		
-		// TODO Atelier 2
-		
-		return coordsOnItinerary;
-	}
-
-	
-	/**
-	 * @param coord
-	 * @return la pi�ce qui se trouve aux coordonn�es indiqu�es
-	 */
-	 PieceModel findPiece(Coord coord) {		// TODO : remettre en "private" apr�s test unitaires
-		PieceModel findPiece = null;
-
-		for(PieceModel piece : this.pieces) {
-
-			if(coord != null && piece.hasThisCoord(coord)) {
-				findPiece = piece;
-				break;
-			}
-		}
-		return findPiece;
-	}
+        boolean isMovePieceOk = false;
+        PieceModel initPiece = this.findPiece(initCoord);
+        if (initPiece != null) {
+            isMovePieceOk = initPiece.isMoveOk(targetCoord, isPieceToTake);
+        }
+        return isMovePieceOk;
+    }
 
 
+    public boolean movePiece(Coord initCoord, Coord targetCoord) {
 
-	/* (non-Javadoc)
-	 * @see java.lang.Object#toString()
-	 * 
-	 * La m�thode toStrong() retourne une repr�sentation 
-	 * de la liste de pi�ces sous forme d'un tableau 2D
-	 * 
-	 */
-	public String toString() {
+        boolean isMovePieceDone = false;
+        PieceModel initPiece = this.findPiece(initCoord);
+        if (initPiece != null) {
+
+            // d�placement pi�ce
+            initPiece.move(targetCoord);
+            isMovePieceDone = true;
+        }
+        return isMovePieceDone;
+    }
+
+    public void removePiece(Coord pieceToTakeCoord) {
+        PieceModel toRemove = null;
+
+        for (PieceModel pm : this.pieces) {
+            if (pm.hasThisCoord(pieceToTakeCoord)) {
+                toRemove = pm;
+                break;
+            }
+        }
+
+        pieces.remove(toRemove);
+    }
 
 
-		String st = "";
-		String[][] damier = new String[ModelConfig.LENGTH][ModelConfig.LENGTH];
+    public List<Coord> getCoordsOnItinerary(Coord initCoord, Coord targetCoord) {
+        return new ArrayList<>(this.findPiece(initCoord).getCoordsOnItinerary(targetCoord));
+    }
 
-		// cr�ation d'un tableau 2D avec les noms des pi�ces � partir de la liste de pi�ces
-		for(PieceModel piece : this.pieces) {
 
-			PieceSquareColor color = piece.getPieceColor();
-			String stColor = (PieceSquareColor.WHITE.equals(color) ? "--B--" : "--N--" );
+    /**
+     * @param coord
+     * @return la pi�ce qui se trouve aux coordonn�es indiqu�es
+     */
+    PieceModel findPiece(Coord coord) {        // TODO : remettre en "private" apr�s test unitaires
+        PieceModel findPiece = null;
 
-			int col = piece.getColonne() -'a';
-			int lig = piece.getLigne() -1;
-			damier[lig][col ] = stColor ;
-		}
+        for (PieceModel piece : this.pieces) {
 
-		// Affichage du tableau formatt�
-		st = "     a      b      c      d      e      f      g      h      i      j\n";
-		for ( int lig = 9; lig >=0 ; lig--) {
-			st += (lig+1) + "  ";
-			for ( int col = 0; col <= 9; col++) {					 
-				String stColor = damier[lig][col];				
-				if (stColor != null) {						
-					st += stColor + "  ";	
-				} 
-				else {
-					st += "-----  ";
-				}
-			}
-			st +="\n";
-		}
-		
-		return "\nDamier du model \n" + st;	
-	}
+            if (coord != null && piece.hasThisCoord(coord)) {
+                findPiece = piece;
+                break;
+            }
+        }
+        return findPiece;
+    }
+
+
+    /* (non-Javadoc)
+     * @see java.lang.Object#toString()
+     *
+     * La m�thode toStrong() retourne une repr�sentation
+     * de la liste de pi�ces sous forme d'un tableau 2D
+     *
+     */
+    public String toString() {
+
+
+        String st = "";
+        String[][] damier = new String[ModelConfig.LENGTH][ModelConfig.LENGTH];
+
+        // cr�ation d'un tableau 2D avec les noms des pi�ces � partir de la liste de pi�ces
+        for (PieceModel piece : this.pieces) {
+
+            PieceSquareColor color = piece.getPieceColor();
+            String stColor = (PieceSquareColor.WHITE.equals(color) ? "--B--" : "--N--");
+
+            int col = piece.getColonne() - 'a';
+            int lig = piece.getLigne() - 1;
+            damier[lig][col] = stColor;
+        }
+
+        // Affichage du tableau formatt�
+        st = "     a      b      c      d      e      f      g      h      i      j\n";
+        for (int lig = 9; lig >= 0; lig--) {
+            st += (lig + 1) + "  ";
+            for (int col = 0; col <= 9; col++) {
+                String stColor = damier[lig][col];
+                if (stColor != null) {
+                    st += stColor + "  ";
+                } else {
+                    st += "-----  ";
+                }
+            }
+            st += "\n";
+        }
+
+        return "\nDamier du model \n" + st;
+    }
 
 }
